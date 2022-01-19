1d3027073121eac021f88a15bea9bf860c12fd2e
Alexey Galashov <AAGalashov@vrn.beeline.ru>
2022-01-19 14:35:49 +0300

    create-call endpoint error

--- src/main/scala/apiservice/calls/Model.scala
+++ src/main/scala/apiservice/calls/Model.scala
@@ -15,7 +15,7 @@ object Model {
   case class CreateCallReq(externalCallId: String, ani: String) extends  Request
   case class CreateCallResp(id: Long) extends Response
 
-  case class CreateTranscribeReq(callId: Long, CreateCallReq: String) extends  Request
+  case class CreateTranscribeReq(callId: Long, transcribe: String) extends  Request
   case class CreateTranscribeResp(id: Long, callId: Long, transcribe: String) extends Response
 
   case class CallInfo(id: Long, externalCallId: String, ani: String, date: Instant) extends  Response
