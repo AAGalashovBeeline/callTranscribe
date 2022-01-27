ecf225ea26145f579c075e392b9758812df8c23c
Alexey Galashov <AAGalashov@vrn.beeline.ru>
2022-01-27 10:08:34 +0300

    LogicsTests v2 clear code
    v2 clear code
    
    v1 primitive test work

--- src/test/scala/apiservice/calls/LogicsTests/LogicsTest.scala
+++ src/test/scala/apiservice/calls/LogicsTests/LogicsTest.scala
@@ -9,42 +9,17 @@ import zio.test._
 import zio.test.Assertion._
 
 object LogicsTest {
-//  val userProfile = new UserProfile("vasiliyshukshin@gmail.com",
-//    "ab31b302-13bb-5e6f-4c89748d28e1",
-//    "beeline",
-//    List(SuperAdminRole),
-//    GroupsCfg(),
-//    "")
-//
-  type HttpServerEnvironment = Configuration with Clock
-  type AppEnvironment = HttpServerEnvironment with HasLogicsClient
 
-  val httpServerEnvironment: ULayer[HttpServerEnvironment] = Configuration.live ++ Clock.live
   val dbTransactor: ULayer[DbTransactor] = Configuration.live >>> DbTransactor.postgres
-  val logicsClient: ULayer[HasLogicsClient] = dbTransactor >>> Logics.live
-
-  val env = logicsClient
-
-//Logger.live ++ MockKeycloak.testLive ++ MockUser.testLive(
-//    Some(new User(Some(userProfile),
-//      Some("tenant")
-//    )))
+  val env: ULayer[HasLogicsClient] = dbTransactor >>> Logics.live
 
   def createCallPBX(externalCallId: String, ani: String) =
     Logics.Service.createCallPBX("sdfd", "asds").run.provideLayer(env)
 
 
-//  def generateUser(email: String): UserRegistration = UserRegistration(email, "Шукшин", "Василий",
-//    Option("Макарович"), true, true, true, true, true
-//  )
-//
-//  def createAccount(reg: UserRegistration) =
-//    Logics.registerUser(reg).run.provideLayer(env)
-
   val test: Spec[Any, TestFailure[Throwable], TestSuccess] = suite("Тесты сервиса Logics")(
     suite("Тесты на проверку Logics")(
       testM("Тест на создание звонка") {
-        val userReg = ZIO.succeed(1 + 1)
 
         assertM(createCallPBX("sdfd", "asds"))(succeeds(anything))
       },
